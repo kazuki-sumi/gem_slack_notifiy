878916a5f1c658a940648f44a83595076a563a54
鷲見和希 <cs5200@icloud.com>
2019-09-28 09:54:03 +0900

    エラーハンドリング

--- lib/slack_notify/client.rb
+++ lib/slack_notify/client.rb
@@ -25,32 +25,40 @@ module SlackNotify
     end
 
     def notify(text, channel = nil)
-      delivery_channels(channel).each do |chan|
-        payload = {
-          text: text,
-          channel: chan,
-          username: @username,
-          icon_url: @icon_url,
-          icon_emoji: @icon_emoji,
-          link_names: @link_names,
-          unfurl_links: @unfurl_links
-        }
-        uri = URI.parse(@webhook_url)
-        req = Net::HTTP::Post.new(uri.request_uri)
-        req["Content-Type"] = "application/json"
-        req.body = payload.to_json
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.request(req)
-      end
-
-      true
+      chan = delivery_channels(channel)
+      payload = {
+        text: text,
+        channel: chan[0],
+        username: @username,
+        icon_url: @icon_url,
+        icon_emoji: @icon_emoji,
+        link_names: @link_names,
+        unfurl_links: @unfurl_links
+      }
+      uri = URI.parse(@webhook_url)
+      req = Net::HTTP::Post.new(uri.request_uri)
+      req["Content-Type"] = "application/json"
+      req.body = payload.to_json
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      response = http.request(req)
+      handle_response(response)
     end
 
     private
 
       def delivery_channels(channel)
         [channel || @channel || "#general"].flatten.compact.uniq
+      end
+
+      def handle_response(response)
+        unless response.code == "200"
+          if response.body.include?("\n")
+            raise SlackNotify::Error
+          else
+            raise SlackNotify::Error.new(response.body)
+          end
+        end
       end
   end
 end
